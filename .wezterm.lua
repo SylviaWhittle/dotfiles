715c1e995fa9a3d585f3e198c05ec379037a3bd6
SylviaWhittle <sylviwhittle@gmail.com>
2024-11-27 14:35:55 +0000

    Add spacing in aerospace and theme in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,10 +7,16 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.max_fps = 120
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
 
 -- For example, changing the color scheme:
 --config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Chalk (dark) (terminal.sexy)'
+--config.color_scheme = 'DanQing (base16)'
+--config.color_scheme = 'Darktooth (base16)'
+--config.color_scheme = 'Decaf (base16)'
+--config.color_scheme = 'Default (dark) (terminal.sexy)'
+config.color_scheme = 'duskfox'
 config.window_background_opacity = 0.9
 
 config.enable_tab_bar = false
@@ -25,6 +31,8 @@ config.font_dirs = {
   '/Users/sylvi/Library/Fonts/',
 }
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+
+config.default_cursor_style = 'BlinkingBlock'
 
 -- and finally, return the configuration to wezterm
 return config
